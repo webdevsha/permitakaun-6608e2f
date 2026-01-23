74c76f06d7c5e0a2ceeb4baeb1c0c9369700211f
webdevsha <43009213+webdevsha@users.noreply.github.com>
2026-01-23 19:19:15 +0800

    Creating middleware utility to manage Supabase session cookies.

---
+++ utils/supabase/middleware.ts
@@ -0,0 +1,46 @@
+import { createServerClient } from '@supabase/ssr'
+import { type NextRequest, NextResponse } from 'next/server'
+
+export const updateSession = async (request: NextRequest) => {
+  try {
+    // Create an unmodified response
+    let response = NextResponse.next({
+      request: {
+        headers: request.headers,
+      },
+    })
+
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          getAll() {
+            return request.cookies.getAll()
+          },
+          setAll(cookiesToSet) {
+            cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
+            response = NextResponse.next({
+              request,
+            })
+            cookiesToSet.forEach(({ name, value, options }) =>
+              response.cookies.set(name, value, options)
+            )
+          },
+        },
+      }
+    )
+
+    // This will refresh session if needed - required for Server Components
+    await supabase.auth.getUser()
+
+    return response
+  } catch (e) {
+    // If you are here, a Supabase client could not be created!
+    return NextResponse.next({
+      request: {
+        headers: request.headers,
+      },
+    })
+  }
+}
